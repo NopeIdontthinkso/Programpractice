16ebc5a34307a56fc22391d35e6f2128ab8dfec5
unknown <tom2005tom2005tom@gmail.com>
2021-12-26 13:10:46 +0800

    add lesson.13 and lesson.14

---
+++ lesson.14/14.py
@@ -0,0 +1,116 @@
+from os import close
+from pycat.core import Color, KeyCode, Sprite, Window, Scheduler
+from random import randint
+
+from pycat.label import Label
+
+window = Window()
+
+class Hp(Label):
+    
+    def on_update(self, dt: float):
+        self.text = 'Hp: '+str(player.hp)
+
+class Score(Label):
+
+    def on_update(self, dt: float):
+        self.text = 'Score: '+str(player.score)
+
+class End(Label):
+
+    def on_create(self):
+        self.text = 'YOU LOST'
+
+class Player(Sprite):
+
+    def on_create(self):
+        self.color = Color.AMBER
+        self.scale = 30
+        self.speed = 10
+        self.x = window.width/2
+        self.y = window.height/2
+        self.hp = 5
+        self.score = 0
+
+    def on_update(self, dt):
+        if window.is_key_pressed(KeyCode.W):
+            self.y += self.speed
+        if window.is_key_pressed(KeyCode.S):
+            self.y -= self.speed
+        if window.is_key_pressed(KeyCode.A):
+            self.x -= self.speed
+        if window.is_key_pressed(KeyCode.D):
+            self.x += self.speed
+        if self.hp <= 1:
+            window.create_label(End, x=window.width/2, y=window.height/2, font_size=100)
+    
+    def on_left_click_anywhere(self):
+        b = window.create_sprite(Bullet, position=self.position)
+        b.point_toward_mouse_cursor()       
+
+class Bullet(Sprite):
+
+    def on_create(self):
+        self.color = Color.AMBER
+        self.scale = 10
+        self.add_tag('Bullet')
+    
+    def on_update(self, dt):
+        self.move_forward(30)
+        if self.is_touching_window_edge():
+            self.delete()
+
+class EnemyBullet(Sprite):
+
+    def on_create(self):
+        self.color = Color.RED
+        self.scale = 10
+    
+    def on_update(self, dt):
+        self.move_forward(30)
+        if self.is_touching_window_edge():
+            self.delete()
+        if self.is_touching_sprite(player):
+            player.hp -= 1 
+            self.delete()
+
+class Enemy(Sprite):
+
+    def on_create(self):
+        self.color = Color.RED
+        self.scale = 30
+        self.speed = 3
+        self.time = 0
+        self.position_number = randint(1, 2)
+        self.add_tag('Enemy')
+        if self.position_number == 1:
+            self.x = randint(70, window.width)
+            self.y = 70
+        else:
+            self.x = 70
+            self.y = randint(70, window.height)
+        self.point_toward_sprite(player)
+
+    def on_update(self, dt):
+        self.time += dt
+        if self.time >= 1:
+            window.create_sprite(EnemyBullet, position=self.position).point_toward_sprite(player)
+            self.time = 0
+        self.move_forward(self.speed)
+        if self.is_touching_window_edge():
+            self.delete() 
+        if self.is_touching_any_sprite_with_tag('Bullet'):
+            for b in self.get_touching_sprites_with_tag("Bullet"):
+                b.delete()
+            player.score += 1 
+            self.delete()
+        
+        
+        
+def create_enemy(dt):
+    window.create_sprite(Enemy)
+player = window.create_sprite(Player)
+Scheduler.update(create_enemy, 2)
+window.create_label(Hp)
+window.create_label(Score, y=620)
+window.run()
