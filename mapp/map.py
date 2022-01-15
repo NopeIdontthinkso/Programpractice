4ee039db7e3166b67ced32ad4f2b3f6f482344d0
unknown <tom2005tom2005tom@gmail.com>
2022-01-15 15:40:07 +0800

    add walls to map

--- mapp/map.py
+++ mapp/map.py
@@ -1,6 +1,7 @@
+from turtle import width
 from pycat.core import Window,Sprite,Color,KeyCode
 
-window = Window()
+window = Window(width = 1002 ,height= 1002)
 
 def nextroom(room , dir):
 
@@ -66,12 +67,58 @@ class Character(Sprite):
                 self.x = window.width - 1
         if window.is_key_pressed(KeyCode.W):
             self.y += self.speed
+            if self.is_touching_any_sprite_with_tag('wall'):
+                self.y -= self.speed
         if window.is_key_pressed(KeyCode.S):
             self.y -= self.speed
+            if self.is_touching_any_sprite_with_tag('wall'):
+                self.y += self.speed
         if window.is_key_pressed(KeyCode.A):
             self.x -= self.speed
+            if self.is_touching_any_sprite_with_tag('wall'):
+                self.x += self.speed
         if window.is_key_pressed(KeyCode.D):
             self.x += self.speed
+            if self.is_touching_any_sprite_with_tag('wall'):
+                self.x -= self.speed
+
+        if self.room == 1:
+            visibility = [True, True, True, True, False, False, True, False, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+        if self.room == 2:
+            visibility = [True, True, True, False, False, False, True, False, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+        if self.room == 3:
+            visibility = [True, True, True, False, False, True, True, False, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+        if self.room == 4:
+            visibility = [True, False, True, True, False, False, True, False, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]        
+        if self.room == 5:
+            visibility = [True, False, True, False, False, False, True, False, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+        if self.room == 6:
+            visibility = [True, False, True, False, False, True, True, False, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+        if self.room == 7:
+            visibility = [True, False, True, True, False, False, True, True, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+        if self.room == 8:
+            visibility = [True, False, True, False, False, False, True, True, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+        if self.room == 9:
+            visibility = [True, False, True, False, False, True, True, True, True]
+            for i in range(9):
+                walls[i].is_visible = visibility[i]
+
 
 class Map(Sprite):
 
@@ -82,8 +129,28 @@ class Map(Sprite):
     def on_update(self, dt):
         self.image = str(player.room) + '.jpg'
 
+class Wall(Sprite):
+
+    def on_create(self):
+        self.color = Color.CYAN
+        self.scale = window.width/3
+        self.layer = 2
+        self.add_tag('wall')
+
 player = window.create_sprite(Character)
 map = window.create_sprite(Map)
+
+w1 = window.create_sprite(Wall, x=window.width/6, y=(window.height*5)/6)
+w2 = window.create_sprite(Wall, x=(window.width*3)/6, y=(window.height*5)/6)
+w3 = window.create_sprite(Wall, x=(window.width*5)/6, y=(window.height*5)/6)
+w4 = window.create_sprite(Wall, x=window.width/6, y=(window.height*3)/6)
+w5 = window.create_sprite(Wall, x=(window.width*3)/6, y=(window.height*3)/6)
+w6 = window.create_sprite(Wall, x=(window.width*5)/6, y=(window.height*3)/6)
+w7 = window.create_sprite(Wall, x=window.width/6, y=window.height/6)
+w8 = window.create_sprite(Wall, x=(window.width*3)/6, y=window.height/6)
+w9 = window.create_sprite(Wall, x=(window.width*5)/6, y=window.height/6)
+
+walls = [w1, w2, w3, w4, w5, w6, w7, w8, w9]
 
 window.run()
         
