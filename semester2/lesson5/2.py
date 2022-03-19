ff49c2e43e01da9312cb131d9e76119181e910f3
unknown <tom2005tom2005tom@gmail.com>
2022-03-20 04:35:50 +0800

    add lesson5

--- semester2/lesson5/2.py
+++ semester2/lesson5/2.py
@@ -1,46 +1,66 @@
-from pycat.core import Window,Sprite
-from random import randint
+from pycat.core import Window,Sprite,KeyCode,Color
+from levelbuild import levelbuild
 
 
 w = Window(is_sharp_pixel_scaling=True)
-ROW = 10
-COLS = 10
-heatmap = []
+
+
 CELL_SIZE = 50
-X0 = 300
-Y0 = 700
+X0 = 100
+Y0 = 500
+LEVEL = [
+            'gggggggggg',
+            'ggwwwwwwgg',
+            'ggwggggwgg',
+            'ggwggggwgg',
+            'ggwggggwgg',
+            'ggwggggggg',
+            'ggwggggggg',
+            'ggwggggggg',
+            'ggwggggggg',
+            'gggggggggg'
+        ]
 
 
-for i in range(ROW):
-    row = []
-    for j in range(COLS):
-        row.append(j+10*i)
-    heatmap.append(row)
-
-class Cell(Sprite):
-
-    def on_left_click(self):
-        print(self.image)
-
-for i in range(ROW):
-    for j in range(COLS):
-        cx = X0 + j*CELL_SIZE
-        cy = Y0 - i*CELL_SIZE
-        c = w.create_sprite(Cell, x=cx, y=cy)
-        numstring = ' '
-        if heatmap[i][j] < 10 :
-            numstring = '00' + str(heatmap[i][j])
-        elif heatmap[i][j] < 100:
-            numstring = '0' + str(heatmap[i][j])
-        else:
-            numstring = str(heatmap[i][j])
-        c.image = 'tiles/tile_' + numstring +'.png'
-        c.scale_to_width(CELL_SIZE)
+LEVEL_D = { 'g': ('tiles/tile_053.png', 'g'),
+            'w': ('tiles/tile_012.png', 'w')
+          }
 
 
+class Player(Sprite):
+
+    def on_create(self):
+        self.scale = 30
+        self.speed = 5
+        self.x = w.width/2
+        self.y = w.height/2
+        self.room = 5
+        self.layer = 2
+        self.hp = 5
+        self.score = 0
+        self.image = 'tiles/tile_190.png'
+        self.scale_to_width(CELL_SIZE)
 
 
+    def on_update(self, dt):
+        if w.is_key_pressed(KeyCode.W):
+            self.y += self.speed
+            if self.is_touching_any_sprite_with_tag('w'):
+                self.y -= self.speed
+        if w.is_key_pressed(KeyCode.S):
+            self.y -= self.speed
+            if self.is_touching_any_sprite_with_tag('w'):
+                self.y += self.speed
+        if w.is_key_pressed(KeyCode.A):
+            self.x -= self.speed
+            if self.is_touching_any_sprite_with_tag('w'):
+                self.x += self.speed
+        if w.is_key_pressed(KeyCode.D):
+            self.x += self.speed
+            if self.is_touching_any_sprite_with_tag('w'):
+                self.x -= self.speed
 
+player = w.create_sprite(Player)
+levelbuild(level=LEVEL, level_d=LEVEL_D, cell_size=CELL_SIZE, x0=X0, y0=Y0, window=w, tag= 'level1')
 
-print(heatmap)
 w.run()
---
+++ semester2/lesson5/levelbuild.py
@@ -0,0 +1,20 @@
+from pycat.core import Window,Sprite
+from typing import List, Dict
+
+def levelbuild(level:List[str], level_d:Dict[str,str], cell_size:int, x0:int, y0:int, tag:str, window: Window):
+    class Cell(Sprite):
+
+        def on_create(self):
+            if self.image == 'tiles/tile_012.png':
+                self.add_tag(tag)
+
+        def on_left_click(self):
+            print(self.image)
+
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            cx = x0 + j*cell_size
+            cy = y0 - i*cell_size
+            c = window.create_sprite(Cell, x=cx, y=cy, image = level_d[level[i][j]][0])
+            c.add_tag(level_d[level[i][j]][1])
+            c.scale_to_width(cell_size)
