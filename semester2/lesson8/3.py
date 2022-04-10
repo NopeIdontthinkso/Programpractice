24555c8571e67e723fdf8f2d983a537721098572
unknown <tom2005tom2005tom@gmail.com>
2022-04-10 12:32:04 +0800

    llis

--- semester2/lesson8/3.py
+++ semester2/lesson8/3.py
@@ -1,18 +1,19 @@
 from turtle import width
 from pycat_turtle import Turtle
 from pycat.core import Window, Point, Sprite
+from random import randint
+
 
 w = Window()
-
-t = w.create_sprite(Turtle, x= w.width/2, y= w.height/2)
+t = w.create_sprite(Turtle)
 
 
 class Rect:
-    def __init__(self ,width ,height):
+    def __init__(self, width, height):
         self.width = width
         self.height =  height
 
-    def draw(self ,x1 ,y1):
+    def draw(self, x1, y1):
         t.x = x1
         t.y = y1
         t.rotation = 0
@@ -25,8 +26,34 @@ class Rect:
         t.draw_forward(self.height)
 
 
-r = Rect(100,200)
+class RobotPart:
 
-for i in range(10):    
-    r.draw(i*100,i*100)
+    def __init__(self, x, y, width, height):
+        r = Rect(width, height)
+        r.draw(x, y)
+
+
+class Robot:
+
+    def __init__(self, x, y, w, h):
+        RobotPart(x, y, w/5, h/5)
+        RobotPart(x, y-2*h/5, w/5, 2*h/5)
+        RobotPart(x, y-4*h/5, w/15, 2*h/5), RobotPart(x+2*w/15, y-4*h/5, w/15, 2*h/5)
+        RobotPart(x-2*w/5, y-h/15, 2*w/5, h/15), RobotPart(x+w/5, y-h/15, 2*w/5, h/15)
+
+        
+# b = RobotPart(500, 500, 100, 100)
+# c = RobotPart(500, 300, 100, 200)
+# d = RobotPart(500, 100, 100/3, 200), RobotPart(500+200/3, 100, 100/3, 200)
+# e = RobotPart(300, 500-100/3, 200, 100/3), RobotPart(600, 500-100/3, 200, 100/3)
+        
+x = 200
+y = 300
+
+for i in range(50):
+    a = randint(50, 200)
+    b = randint(50, 200)
+    Robot(x, y, a, b)
+    x += a
+
 w.run()
