c1425c8f7fa91b93df6f752a7c450c78726e663c
unknown <tom2005tom2005tom@gmail.com>
2022-06-05 11:42:06 +0800

    aspmcp

--- semester2/final_project/final_project.py
+++ semester2/final_project/final_project.py
@@ -10,6 +10,9 @@ w = Window(enforce_window_limits=False)
 class PlayerState(Enum):
     WAIT = auto()
     JUMP = auto()
+    WALK = auto()
+    FLY = auto()
+    
 
 
 class HollowKnight(Sprite):
@@ -18,31 +21,70 @@ class HollowKnight(Sprite):
     def on_create(self):
         self.scale = 50
         self.color = Color.RED
-        self.state = PlayerState.WAIT
+        self.state = PlayerState.JUMP
         self.g = 0.2
-        self.fall = 0
+        self.y_speed = 0
         self.jump_time = 0
+        self.speed = 10
     
     def on_update(self, dt):
-        if self.state == PlayerState.JUMP:
-            self.y -= self.fall
-            if w.is_key_down(KeyCode.SPACE) and self.jump_time < 2:
-                self.fall = -8
+        print(self.state)
+        if self.state is PlayerState.JUMP:
+            self.y -= self.y_speed
+            if w.is_key_pressed(KeyCode.RIGHT) or w.is_key_pressed(KeyCode.LEFT):
+                self.state = PlayerState.FLY
+            if w.is_key_down(KeyCode.UP) and self.jump_time < 2:
+                self.y_speed = -8
                 self.jump_time += 1
-            if self.fall < 10:
-                self.fall += self.g
-            if self.is_touching_any_sprite_with_tag('floor') and self.fall>0:
+            if self.y_speed < 10:
+                self.y_speed += self.g
+            if self.is_touching_any_sprite_with_tag('floor') and self.y_speed>0:
                 self.state = PlayerState.WAIT
-                self.fall = 0
+                self.y_speed = 0
                 self.jump_time = 0
-        if self.state == PlayerState.WAIT:
-            if w.is_key_down(KeyCode.SPACE):
-                self.state = PlayerState.JUMP
-                self.fall = -8
-                self.jump_time += 1
-        if w.is_key_pressed(KeyCode.LEFT):
-            pass
 
+        #jump state
+        if self.state is PlayerState.WAIT:
+            if w.is_key_down(KeyCode.UP):
+                self.state = PlayerState.JUMP
+                self.y_speed = -8
+                self.jump_time += 1
+            if self.move_left_right_if_press_keys():
+                self.state = PlayerState.WALK
+        
+        # waiting state
+        if self.state is PlayerState.WALK:
+            if w.is_key_pressed(KeyCode.UP):
+                self.state = PlayerState.FLY
+            if not self.move_left_right_if_press_keys():
+                self.state = PlayerState.WAIT
+
+        #walk 
+        if self.state is PlayerState.FLY:
+            self.y -= self.y_speed
+            if w.is_key_down(KeyCode.UP) and self.jump_time < 2:
+                self.y_speed = -8
+                self.jump_time += 1
+            if self.y_speed < 10:
+                self.y_speed += self.g
+            if self.is_touching_any_sprite_with_tag('floor') and self.y_speed>0:
+                self.state = PlayerState.WAIT
+                self.y_speed = 0
+                self.jump_time = 0
+            if not self.move_left_right_if_press_keys():
+                self.state = PlayerState.JUMP
+
+        #fly
+
+
+    def move_left_right_if_press_keys(self):
+        if w.is_key_pressed(KeyCode.LEFT):
+            self.x -= self.speed
+            return True
+        if w.is_key_pressed(KeyCode.RIGHT):
+            self.x += self.speed
+            return True
+        return False
 
 class Backgound_floor(Sprite):
 
