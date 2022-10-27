5549dd17ce6170635368634fae9f2638f5ddeec4
unknown <tom2005tom2005tom@gmail.com>
2022-10-27 12:54:53 +0800

    a

---
+++ cs/1.py
@@ -0,0 +1,27 @@
+import threading
+var = 0
+def task1():
+    global var
+    a = 0
+    print('11:', var)
+    for i in range(30000):
+        a += 1
+    var += 1
+    print('12:', var)
+
+def task2():
+    global var
+    a = 0
+    print('21:', var)
+    for i in range(200000):
+        a += 1
+    var += 2
+    print('22:', var)
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=task1)
+    t2 = threading.Thread(target=task2)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
